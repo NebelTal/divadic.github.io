8d566b10777d96a43870dbbdce6e393658552f90
NebelTal <triptych289@gmail.com>
2025-05-17 01:45:14 +0900

    Updates

---
+++ precache-manifest.0169b1d3aac661a877561a1e60a67c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a960c3d64e5a338e6db198fff6a34dd",
+    "url": "/divadic.github.io/index.html"
+  },
+  {
+    "revision": "4d7b4983d6a82db2ecb3",
+    "url": "/divadic.github.io/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/divadic.github.io/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/divadic.github.io/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "4d7b4983d6a82db2ecb3",
+    "url": "/divadic.github.io/static/js/main.1868c1e1.chunk.js"
+  },
+  {
+    "revision": "361cc68a89cefdf7429c",
+    "url": "/divadic.github.io/static/js/runtime~main.daa7dd3d.js"
+  }
+]);
