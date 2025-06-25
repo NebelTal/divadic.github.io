4c40e025592ab6f0abe8d5326ffd554d2945f36e
NebelTal <triptych289@gmail.com>
2025-06-25 11:44:10 +0900

    Updates

--- precache-manifest.6a329871d946035be84f6337a472f5a3.js
+++ precache-manifest.6a329871d946035be84f6337a472f5a3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "40033d53d3577d2dbf2f8738b9812b88",
+    "revision": "8f7bee7d2ef02e4fd52c6a0635fcb7b8",
     "url": "/divadic.github.io/index.html"
   },
   {
-    "revision": "35ebd1da7003f85a9032",
+    "revision": "c33290e3e0b5d71c7017",
     "url": "/divadic.github.io/static/css/main.d577c0ac.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/divadic.github.io/static/js/3.ba8b7f13.chunk.js"
   },
   {
-    "revision": "35ebd1da7003f85a9032",
-    "url": "/divadic.github.io/static/js/main.06ec2e98.chunk.js"
+    "revision": "c33290e3e0b5d71c7017",
+    "url": "/divadic.github.io/static/js/main.ea29d7b9.chunk.js"
   },
   {
     "revision": "f396aa9b8b8f94f249b1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/divadic.github.io/precache-manifest.da7ce91b14c432475a99c6b18e781645.js"
+  "/divadic.github.io/precache-manifest.6a329871d946035be84f6337a472f5a3.js"
 );
 
 self.addEventListener('message', (event) => {
